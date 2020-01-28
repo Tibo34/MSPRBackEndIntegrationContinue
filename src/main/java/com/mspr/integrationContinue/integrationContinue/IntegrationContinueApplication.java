07bf1d4baaff7c4ba1895010bbd74b85511cd313
Molina Thibaut <thibautmolina@yahoo.fr>
2020-01-28 12:15:30 +0100

    api

---
+++ src/main/java/com/mspr/integrationContinue/integrationContinue/IntegrationContinueApplication.java
@@ -0,0 +1,13 @@
+package com.mspr.integrationContinue.integrationContinue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntegrationContinueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntegrationContinueApplication.class, args);
+	}
+
+}
